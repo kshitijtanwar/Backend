31e9b3e6bd9a17bee0dbd7029ca10bde2598b2e4
Kshitij <kshitij.tanwar2002@gmail.com>
2024-05-06 02:02:34 +0530

    fix: return in async handler | user route setup

--- src/app.js
+++ src/app.js
@@ -12,7 +12,13 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+//route declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,12 @@
-const asyncHandler = (fn = async (req, res, next) => {
+const asyncHandler = (fn) => async (req, res, next) => {
     try {
-        await fn(req, res, next);
+        return await fn(req, res, next);
     } catch (error) {
         res.status(err.code || 500).json({
             success: false,
             message: err.message,
         });
     }
-});
+};
 
 export { asyncHandler };
