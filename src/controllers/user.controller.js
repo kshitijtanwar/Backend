6ff0edcfa35e77eea561ed909aa1aa7f5e481271
Kshitij <kshitij.tanwar2002@gmail.com>
2024-05-06 03:11:29 +0530

    fix: minor fixes in multer

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,7 +14,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required.");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }],
     });
     if (existedUser) {
@@ -31,7 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
     if (!avatar) throw new ApiError(400, "Avatar is required");
 
     const user = await User.create({
-        username: username.toLowercase(),
+        username,
         fullName,
         email,
         avatar: avatar.url,
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,12 +2,13 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "/public/assets");
+        cb(null, "./public/assets");
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + "-" + uniqueSuffix);
+        cb(null, file.originalname);
     },
 });
 
-export const upload = multer({ storage: storage });
+export const upload = multer({
+    storage,
+});
